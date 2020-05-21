b09eed178a7f3f120cdde4f179fafe4755f28bf6
Niklas Rother <info@niklas-rother.de>
2020-05-22 00:12:56 +0200

    Create adapter from template

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+    "eq3-ccrtble adapter settings": {
+        "en": "Adapter settings for eq3-ccrtble",
+        "de": "Adaptereinstellungen für eq3-ccrtble",
+        "ru": "Настройки адаптера для eq3-ccrtble",
+        "pt": "Configurações do adaptador para eq3-ccrtble",
+        "nl": "Adapterinstellingen voor eq3-ccrtble",
+        "fr": "Paramètres d'adaptateur pour eq3-ccrtble",
+        "it": "Impostazioni dell'adattatore per eq3-ccrtble",
+        "es": "Ajustes del adaptador para eq3-ccrtble",
+        "pl": "Ustawienia adaptera dla eq3-ccrtble",
+        "zh-cn": "eq3-ccrtble的适配器设置"
+    },
+    "option1": {
+        "en": "option1",
+        "de": "Option 1",
+        "ru": "Опция 1",
+        "pt": "Opção 1",
+        "nl": "Optie 1",
+        "fr": "Option 1",
+        "it": "opzione 1",
+        "es": "Opción 1",
+        "pl": "opcja 1",
+        "zh-cn": "选项1"
+    },
+    "option2": {
+        "en": "option2",
+        "de": "Option 2",
+        "ru": "option2",
+        "pt": "opção 2",
+        "nl": "Optie 2",
+        "fr": "Option 2",
+        "it": "opzione 2",
+        "es": "opcion 2",
+        "pl": "Opcja 2",
+        "zh-cn": "选项2"
+    }
+};
